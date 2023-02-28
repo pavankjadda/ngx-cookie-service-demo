1b5787b79f342f695f8fbb71ee722a03d3bcc9c9
pavankjadda <17564080+pavankjadda@users.noreply.github.com>
2023-02-28 09:24:41 -0500

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, inject, OnInit, VERSION } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { v4 as uuidv4 } from 'uuid';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+  cookieService = inject(CookieService);
   cookieValue = '';
-  constructor(public cookieService: CookieService) {
+
+  ngOnInit(): void {
     this.cookieService.set('X-Auth-Token', uuidv4());
     this.cookieValue = this.cookieService.get('X-Auth-Token');
   }
